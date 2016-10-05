6671955ede638b688968c3e675192a65d9defb08
ArialBlack <andreyturik@gmail.com>
2016-10-05 16:10:48 +0300

    works changes

--- themes/laba/dev-templates/page--front.php
+++ themes/laba/dev-templates/page--front.php
@@ -1,35 +1,29 @@
 <a id="showMenu" class="logo navbar-btn pull-left" href="" title="Меню">
-	<img src="/sites/all/themes/lab/images/about-devider.png" alt="Меню" />
-	<p class="text-center">Меню</p>
+	<img src="/sites/all/themes/laba/images/menu.png" alt="Меню" />
 </a>
 
 <div id="perspective" class="perspective effect-laydown">
 	<div id="perspective-container">
 		<div id="perspective-wrapper"><!-- wrapper needed for scroll -->
 
-<div id="ip-container" class="ip-container">
-	<!--(bake parts/preloader.php)-->
+			<div id="ip-container" class="ip-container">
+				<!--(bake parts/preloader.php)-->
 
-	<?php
-		$block = block_load('laba', 'frontworksblock');
-		$block_content = _block_render_blocks(array($block));
-		$build = _block_get_renderable_array($block_content);
-		print drupal_render($build);
-	?>
+				<?php
+					$block = block_load('laba', 'frontworksblock');
+					$block_content = _block_render_blocks(array($block));
+					$build = _block_get_renderable_array($block_content);
+					print drupal_render($build);
+				?>
+			</div>
 
+		</div>
+	</div>
 
-</div>
-
-</div>
-</div>
-<nav class="outer-nav left vertical">
-	<a href="/" class="icon-home">Головна</a>
-	<a href="/works" class="icon-news">Роботи</a>
-	<a href="/blog" class="icon-image">Блог</a>
-	<a href="/contacts" class="icon-mail">Контакти</a>
-	<!--<a href="#" class="icon-upload">Uploads</a>
-    <a href="#" class="icon-star">Favorites</a>
-
-    <a href="#" class="icon-lock">Security</a>-->
-</nav>
+	<nav class="outer-nav left vertical">
+		<a href="/" class="icon-home">Головна</a>
+		<a href="/works" class="icon-news">Роботи</a>
+		<a href="/blog" class="icon-image">Блог</a>
+		<a href="/contacts" class="icon-mail">Контакти</a>
+	</nav>
 </div><!-- /perspective -->
